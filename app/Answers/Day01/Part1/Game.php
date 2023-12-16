1eda3292208013a1855c0f8c4c9e77b1b0db5697
mentosmenno2 <mentosmenno2@hotmail.com>
2023-12-16 12:59:52 +0100

    add comments for day 1

--- app/Answers/Day01/Part1/Game.php
+++ app/Answers/Day01/Part1/Game.php
@@ -14,30 +14,39 @@ final class Game extends AbstractGame
 
 	protected function prepare_data(string $file_data): void
 	{
+		// Convert the text to an array of text lines.
 		$lines = explode(PHP_EOL, $file_data);
+
+		// For text line in the array of text lines, convert it to an array of characters.
 		$this->lines = array_map('str_split', $lines);
 	}
 
 	protected function start(): void
 	{
+		// Create a variable called total, that keeps track of the sum of the numbers on every line.
 		$total = 0;
+
+		// Loop over all text lines.
 		foreach ($this->lines as $index => $line) {
+
+			// Extract only the numbers from the text.
 			$digits = array_filter($line, function (string $character) {
 				return is_numeric($character);
 			});
 
+			// Get the first and last number. If there is no last number, use the first number as the last number.
 			$first = array_shift($digits);
 			$last = array_pop($digits);
 			$last = $last ?: $first;
 
+			// Combine the two numbers into one number.
 			$line_digits = (int) ( $first . $last );
-			$total += $line_digits;
 
-			$this->output->echo_line(
-				sprintf('The total of line %d is %d.', $index + 1, $line_digits)
-			);
+			// Add the number of this line to the total variable.
+			$total += $line_digits;
 		}
 
+		// Display the answer.
 		$this->output->echo_line(
 			sprintf('The total of all the numbers is %d.', $total)
 		);
--- app/Answers/Day01/Part2/Game.php
+++ app/Answers/Day01/Part2/Game.php
@@ -38,25 +38,30 @@ final class Game extends AbstractGame
 
 	protected function prepare_data(string $file_data): void
 	{
+		// Convert the text to an array of text lines.
 		$lines = explode(PHP_EOL, $file_data);
 		$this->lines = $lines;
 	}
 
 	protected function start(): void
 	{
+		// Create a variable called total, that keeps track of the sum of the numbers on every line.
 		$total = 0;
+
+		// Loop over all text lines.
 		foreach ($this->lines as $index => $line) {
+			// Get the first and last number.
 			$first_digit = $this->getFirstDigit($line);
 			$lastDigit = $this->getLastDigit($line);
+
+			// Combine the two numbers into one number.
 			$totalDigit = $first_digit . $lastDigit;
 
-			$this->output->echo_line(
-				sprintf('The total of line %d is %d.', $index, $totalDigit)
-			);
-
+			// Add the number of this line to the total variable.
 			$total += (int) $totalDigit;
 		}
 
+		// Display the answer.
 		$this->output->echo_line(
 			sprintf('The total of all the numbers is %d.', $total)
 		);
@@ -64,10 +69,16 @@ final class Game extends AbstractGame
 
 	protected function getFirstDigit(string $line): ?string
 	{
+		// We loop for every character in the line text.
 		for ($i=0; $i < strlen($line); $i++) {
+			// Make a string starting from the first character of the line, with the length of the total amount of loops.
 			$string_part = substr($line, 0, $i + 1);
+
+			// Now check all the numbers or written numbers.
 			foreach ($this->map as $key => $value) {
+				// If the text ends with the (written) number you are currently checking, return it.
 				if (str_ends_with($string_part, (string) $key)) {
+					// Return the (written) number.
 					return $value;
 				}
 			}
@@ -77,10 +88,16 @@ final class Game extends AbstractGame
 
 	protected function getLastDigit(string $line): ?string
 	{
+		// We loop for every character in the line text.
 		for ($i=0; $i < strlen($line); $i++) {
+			// Make a string starting from the last character of the line, with the inverted length of the total amount of loops.
 			$string_part = substr($line, strlen($line) - 1 - $i, strlen($line));
+
+			// Now check all the numbers or written numbers.
 			foreach ($this->map as $key => $value) {
+				// If the text starts with the (written) number you are currently checking, return it.
 				if (str_starts_with($string_part, (string) $key)) {
+					// Return the (written) number.
 					return $value;
 				}
 			}
